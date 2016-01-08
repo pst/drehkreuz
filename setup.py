72b76cf77ee950c27de8bd744b84186ef9bde127
Philipp Strube <pst@kubestack.com>
2016-01-08 17:21:01 +0100

    Upgrade webassets and use included libsass filter and pin libsass<0.10

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='BER',
-    version='0.1.6',
+    version='0.1.7',
     description='BER content aggregation hub',
     author='Philipp Strube',
     author_email='pst@cloudcontrol.de',
@@ -14,8 +14,8 @@ setup(
         "tornado==4.0.2",
         "PyYAML==3.11",
         "Jinja2==2.7.3",
-        "webassets==0.10.1",
-        "webassets-libsass==0.1",
+        "webassets==0.11.1",
+        "libsass<0.10",
         "feedparser==5.2.1",
     ]
 )
