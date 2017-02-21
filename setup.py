b334da345bfa1ccf3c846ec9cac2cc4f21921cbb
Philipp Strube <pst@kubestack.com>
2017-02-21 18:05:50 +0100

    Upgrade dependency versions

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ from setuptools import setup
 
 setup(
     name='BER',
-    version='0.1.7',
+    version='0.1.8',
     description='BER content aggregation hub',
     author='Philipp Strube',
     author_email='pst@cloudcontrol.de',
     url='',
     packages=['BER'],
     install_requires=[
-        "tornado==4.0.2",
+        "tornado==4.4.2",
         "PyYAML==3.11",
         "Jinja2==2.7.3",
-        "webassets==0.11.1",
-        "libsass<0.10",
+        "webassets==0.12.1",
+        "libsass==0.12.3",
         "feedparser==5.2.1",
     ]
 )
